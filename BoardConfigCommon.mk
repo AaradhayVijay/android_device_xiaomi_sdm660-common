6cfdbfce1f1373cd58b23da19a1b5aeec9b55a32
Nolen Johnson <johnsonnolen@gmail.com>
2022-12-27 15:25:35 -0500

    sdm660-common: Explicitly disable AVB
    * This allows infra to sign successfully, I hate it, but it builds,
      boots, and works fine enough as a workaround.
    
    * The underlying issue is that this flag causes avbtool args and location
      to be set in `misc_info.txt` whereas before it was not.
    
    Change-Id: I7bdf237ec30d692c7d8b5c626a6e761e2c1a2593

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,10 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 # Treble
 BOARD_VNDK_VERSION := current
 
+# Verity
+# Only needed for signing
+BOARD_AVB_ENABLE := false
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
